18227627820c67fc09cf7a942d6be3d6bd203749
Jonathan Kim <jkimbo@gmail.com>
2013-04-01 15:40:14 +0100

    Removed db connect sample

--- dBconnect.sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db     =   "DATABASE";
-    $host   =   "localhost";
-    $user   =   "USERNAME";
-    $pass   =   "PASSWORD";
-    $cid    =   mysql_connect($host,$user,$pass);
-    $dbok   =   mysql_select_db($db,$cid);
-?>
